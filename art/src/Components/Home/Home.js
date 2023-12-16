6f893d5c9c9cbe5a39883df496c3d23292ad5cac
nthenyakyalo202 <nthenyakyalo202@gmail.com>
2023-12-16 23:10:58 +0300

    art

--- art/src/Components/Home/Home.js
+++ art/src/Components/Home/Home.js
@@ -1,27 +1,18 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
+import Banner from '../Image/lion.jpeg'
+import '../Home/Home.css'
 
 const Home = () => {
   return (
-    <div className='home'>
+    <div className='home-container'>
       <Navbar />
-      <div className="home-text-section">
-          <h1 className="primary-heading">
-          Digital Art  <br />
-          Web & Digital Canvas
-          </h1>
-          <p className="primary-text">
-          Welcome to our digital art haven! Explore a diverse collection of 
-          stunning digital artworks created by talented artists. From vibrant 
-          illustrations to mesmerizing digital paintings, our gallery showcases 
-          the limitless possibilities of digital art. Join our community, unleash 
-          your creativity, and discover the beauty of the digital canvas. Start your 
-          artistic journey with us today!
-          </p>
-          <button className="secondary-button">
-            Explore
-          </button>
+      <div className='home-banner-container'>
+        <div className="home-image-section">
+          <img src={Banner} alt="" />
         </div>
+        </div>
+        
     </div>
   )
 }
--- art/src/Components/Navbar/Navbar.js
+++ art/src/Components/Navbar/Navbar.js
@@ -2,6 +2,9 @@ import {Component} from 'react';
 import "../Navbar/Navbar.css"
 import logo from '../Image/Rectangle 1.png'
 import logoo from '../Image/ART.png'
+import Banner from '../Image/lion.jpeg'
+
+
 
 
 class Navbar extends Component{
@@ -49,6 +52,26 @@ class Navbar extends Component{
         
      </div>
      </nav>
+     
+     <div className="home-text-section">
+     
+          <h1 className="primary-heading">
+          Digital Art  <br />
+          Web & Digital Canvas
+          </h1>
+          <p className="primary-text">
+          Welcome to our digital art haven! Explore a diverse collection of 
+          stunning digital artworks created by talented artists. From vibrant 
+          illustrations to mesmerizing digital paintings, our gallery showcases 
+          the limitless possibilities of digital art. Join our community, unleash 
+          your creativity, and discover the beauty of the digital canvas. Start your 
+          artistic journey with us today!
+          </p>
+          
+          <button className="secondary-button">
+            Explore
+          </button>
+        </div>
      
     </>
   )
