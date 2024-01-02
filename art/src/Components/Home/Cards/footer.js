909df76de49b13e7045baf14b4b38cb78ffdb6e2
nthenyakyalo202 <nthenyakyalo202@gmail.com>
2024-01-03 00:36:00 +0300

    art

--- art/src/Components/Home/Cards/footer.js
+++ art/src/Components/Home/Cards/footer.js
@@ -5,19 +5,7 @@ const footer = () => {
     <div className='footer'>
         <div>
             <div>
-                <div>
-                    <h4>For buiness</h4>
-                    <a href='/employers'>
-                        <p>Employers</p>
-                    </a>
-                    <a href='/healthplan'>
-                        <p>health Plan</p>
-                    </a>
-                    <a href='/individuals'>
-                        <p>Individuals</p>
-                    </a>
-                    
-                </div>
+                
                 <div>
                     <h4>Resources</h4>
                 <a href='/employers'>
